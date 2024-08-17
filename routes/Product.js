270fc9821ddf7c86accad23dd35c326a9f1877a9
chaudhuree <chaudhuree00@gmail.com>
2024-08-17 12:30:48 +0600

    installed: express-async-handler fix: import and export system

--- routes/Product.js
+++ routes/Product.js
@@ -1,7 +1,17 @@
-import express from 'express';
+const express = require('express');
 
-
+const { createProduct,
+  getAllProducts,
+  getProduct,
+  updateProduct,
+  deleteProduct } = require('../controllers/Product');
 const router = express.Router();
 
+router.get('/products', getAllProducts);
+router.post('/product', createProduct);
+router.get('/product/:id', getProduct);
+router.put('/product/:id', updateProduct);
+router.delete('/product/:id', deleteProduct);
 
-export default router;
+
+module.exports = router;
